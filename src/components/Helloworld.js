098f6553f8288bac77d8089292eaaa4a73951bcf
[your <rifai.it@gmail.com>
2019-08-06 11:52:06 +0200

    first comment

---
+++ src/components/Helloworld.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Helloworld = () => {
+    return (
+        <div>
+        <h1>Hello Class!</h1>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+             sed diam nonumy eirmod tempor invidunt ut labore et 
+             Stet clita kasd gubergren, no sea takimata sanctus est
+             </p>
+        </div>
+    )
+}
+
+export default Helloworld
+
